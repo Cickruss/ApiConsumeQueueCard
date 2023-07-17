6c8645596ec250116a27a7b798b663b6cd7e9436
Cickruss <icaro.o.macedo@gmail.com>
2023-07-17 17:01:40 -0300

    Colocando variaveis de ambiente

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,10 @@
+require('dotenv').config()
 const mongoose = require("mongoose")
 
 async function main(params) {
     try {
         mongoose.set("strictQuery", true)
-        await mongoose.connect(
-            "mongodb+srv://Cickruss:7JeuCcZIt2BT7J4f@cluster0.zie2z5d.mongodb.net/?retryWrites=true&w=majority"
-        )
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Database connected!")
     } catch (error) {
         console.log(`Error: ${error}`)
